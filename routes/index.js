9f0e61a151ea7527e89562111f1131e1f73e2f80
reubenson <reubenson@gmail.com>
2016-01-20 14:39:58 -0500

    commented out extraneous routes in index.js

--- routes/index.js
+++ routes/index.js
@@ -15,25 +15,25 @@ router.get('/', function(req, res, next) {
   });
 });
 
-router.get('/birdr', function(req, res, next) {
-  var jade = require('jade');
-  res.render('projects/show', {title: "Birdr", templateRender: jade.renderFile});
-});
-
-router.get('/Git-R', function(req, res, next) {
-  var jade = require('jade');
-  res.render('projects/show', {title: "Git-R", templateRender: jade.renderFile});
-});
-
-router.get('/qcvg', function(req, res, next) {
-  var jade = require('jade');
-  res.render('projects/show', {title: "QCVG", templateRender: jade.renderFile});
-});
-
-router.get('/private-chronology', function(req, res, next) {
-  var jade = require('jade');
-  res.render('projects/show', {title: "Private Chronology", templateRender: jade.renderFile});
-});
+// router.get('/birdr', function(req, res, next) {
+//   var jade = require('jade');
+//   res.render('projects/show', {title: "Birdr", templateRender: jade.renderFile});
+// });
+//
+// router.get('/Git-R', function(req, res, next) {
+//   var jade = require('jade');
+//   res.render('projects/show', {title: "Git-R", templateRender: jade.renderFile});
+// });
+//
+// router.get('/qcvg', function(req, res, next) {
+//   var jade = require('jade');
+//   res.render('projects/show', {title: "QCVG", templateRender: jade.renderFile});
+// });
+//
+// router.get('/private-chronology', function(req, res, next) {
+//   var jade = require('jade');
+//   res.render('projects/show', {title: "Private Chronology", templateRender: jade.renderFile});
+// });
 
 
 
