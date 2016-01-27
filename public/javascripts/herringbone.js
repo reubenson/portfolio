7a259a6bf2bbeb299b6d84509712f9d45c1f05a5
reubenson <reubenson@gmail.com>
2016-01-27 10:40:57 -0500

    herringbone refactor and color change

--- public/javascripts/herringbone.js
+++ public/javascripts/herringbone.js
@@ -1,13 +1,15 @@
 var direction = 1;
 var max_offset = 60;
 var counter = -30;
-var margin = 0;
+var margin = -5;
 var fill_screen = true;
 var animate = true;
+var col_width = 100;
+var v_spacing = 20;
+var ctx;
 
 
 $(function(){
-  // $('body').css('margin',0);
   drawHerringbone();
   animate = false;
   $(window).click(function(){
@@ -21,70 +23,25 @@ $(function(){
 function drawHerringbone() {
   if (!animate) { return }
   var canvas = document.querySelector("canvas");
-  var ctx = canvas.getContext("2d");
+  ctx = canvas.getContext("2d");
   canvas.width = $(window).width();
   canvas.height = $(window).height();
   var height = $('canvas').height()-margin*2;
   var width = $('canvas').width()-margin*2;
   ctx.clearRect(0,0,width,height);
 
-  var n_columns = 20;
-  var col_width = width/n_columns;
+  var n_columns = Math.round(width/col_width);
+  col_width = width/n_columns;
+
   var offset = counter;
-  var v_spacing = 20;
 
   for (var i = 0; i < n_columns+1; i++) {
-    if (i%2) {
-      draw_line(ctx , {x:margin+i*col_width,y:margin-max_offset} , {x:margin+i*col_width,y:margin+height+max_offset-v_spacing+3});
-    } else {
-      draw_line(ctx , {x:margin+i*col_width,y:margin-max_offset} , {x:margin+i*col_width,y:margin+height+max_offset-v_spacing+3});
-    }
+    drawHerringboneSpine(i,height,width,offset);
 
     if (i == n_columns) {break;}
 
-    for (var j = margin-max_offset; j < height+max_offset; j+=v_spacing) {
-      if (i%2){
-        draw_line(ctx , {x:margin+i*col_width,y:j-offset} , {x:margin+(i+1)*col_width,y:j+offset});
-      } else {
-        draw_line(ctx , {x:margin+i*col_width,y:j+offset} , {x:margin+(i+1)*col_width,y:j-offset});
-      }
-    }
-  }
-  counter += direction;
-  if (counter == max_offset) { direction *= -1 }
-  else if (counter == -max_offset) {direction *= -1 }
-}
+    drawHerringboneRibs(i,height,width,offset);
 
-function draw() {
-  var canvas = document.querySelector("canvas");
-  var ctx = canvas.getContext("2d");
-  canvas.width = $(window).width()-20;
-  canvas.height = $(window).height()-20;
-  var height = $('canvas').height()-margin*2;
-  var width = $('canvas').width()-margin*2;
-  ctx.clearRect(0,0,width,height);
-
-  var n_columns = 18;
-  var col_width = width/n_columns;
-  var offset = counter;
-  var v_spacing = 10;
-
-  for (var i = 0; i < n_columns+1; i++) {
-    if (i%2) {
-      draw_line(ctx , {x:margin+i*col_width,y:margin+max_offset-offset} , {x:margin+i*col_width,y:margin+height-max_offset-offset-v_spacing+3});
-    } else {
-      draw_line(ctx , {x:margin+i*col_width,y:margin+max_offset+offset} , {x:margin+i*col_width,y:margin+height-max_offset+offset-v_spacing+3});
-    }
-
-    if (i == n_columns) {break;}
-
-    for (var j = max_offset+margin; j < height-max_offset; j+=v_spacing) {
-      if (i%2){
-        draw_line(ctx , {x:margin+i*col_width,y:j-offset} , {x:margin+(i+1)*col_width,y:j+offset});
-      } else {
-        draw_line(ctx , {x:margin+i*col_width,y:j+offset} , {x:margin+(i+1)*col_width,y:j-offset});
-      }
-    }
   }
   counter += direction;
   if (counter == max_offset) { direction *= -1 }
@@ -96,5 +53,34 @@ function draw_line(ctx,start,end) {
   ctx.moveTo(start.x,start.y);
   ctx.lineTo(end.x,end.y);
   ctx.lineWidth = 3;
+  ctx.strokeStyle = "#c6c6c6";
   ctx.stroke();
+}
+
+function drawHerringboneSpine(i,height,width,offset){
+  var x_pos = margin+i*col_width;
+  var y_start = margin-max_offset;
+  var y_end = margin+height+max_offset-v_spacing+3;
+
+  if (!fill_screen) {
+    if (i%2) { y_start = margin+max_offset-offset; }
+    else     { y_start = margin+max_offset+offset; }
+    y_end = y_start + height - max_offset*2 - v_spacing - margin + 3;
+  }
+
+  draw_line(ctx , {x:x_pos,y:y_start} , {x:x_pos,y:y_end});
+}
+
+function drawHerringboneRibs(i,height,width,offset){
+  var x_range = fill_screen ? [margin-max_offset , height+max_offset] : [margin+max_offset , height-max_offset];
+
+  for (var j = x_range[0]; j < x_range[1]; j+=v_spacing) {
+    var x_start  = margin + i*col_width;
+    var x_end    = margin + (i+1)*col_width;
+    var y_top    = j + offset;
+    var y_bottom = j - offset;
+
+    if (i%2){ draw_line(ctx , {x:x_start,y:y_bottom} , {x:x_end,y:y_top}); }
+    else {    draw_line(ctx , {x:x_start,y:y_top} , {x:x_end,y:y_bottom}); }
+  }
 }
