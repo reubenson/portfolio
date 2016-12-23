e1d5f416f0eeb2225eb48bf2210a9e8f323d3204
reubenson <reubenson@gmail.com>
2016-12-23 18:18:59 -0500

    serve defunct private chronology pages as static public assets

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var projects = require('./routes/projects');
-var privatechronology = require('./routes/privatechronology')
 
 var app = express();
 
@@ -27,7 +26,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/projects', projects);
-app.use('/privatechronology',privatechronology);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
